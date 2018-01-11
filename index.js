fe54ea7e1a28ff3142113bdb718866127418b0c8
NorthernMan54 <buyminivan-northern@yahoo.ca>
2018-01-10 22:20:32 -0500

    Further cleanup

--- index.js
+++ index.js
@@ -10,7 +10,8 @@
 
 var eco = require('./lib/eco.js');
 var debug = require('debug')('EcoPlug');
-var Accessory, Service, Characteristic, UUIDGen;
+var Accessory, Service, Characteristic, UUIDGen, HAPServer;
+var accessories = [];
 
 module.exports = function(homebridge) {
 
@@ -18,15 +19,13 @@ module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
   UUIDGen = homebridge.hap.uuid;
+  HAPServer = homebridge.hap.HAPServer;
 
   homebridge.registerPlatform("homebridge-ecoplugs", "EcoPlug", EcoPlugPlatform);
 }
 
 function EcoPlugPlatform(log, config, api) {
   this.log = log;
-  this.config = config;
-  //    this.plugs = this.config.plugs || [];
-  this.accessories = [];
   this.cache_timeout = 10; // seconds
   this.refresh = config['refresh'] || 10; // Update every 10 seconds
 
@@ -40,7 +39,7 @@ EcoPlugPlatform.prototype.configureAccessory = function(accessory) {
   var accessoryId = accessory.context.id;
   this.log("configureAccessory", accessoryId, accessory.context.name);
   this.setService(accessory);
-  this.accessories[accessoryId] = accessory;
+  accessories[accessoryId] = accessory;
 }
 
 EcoPlugPlatform.prototype.didFinishLaunching = function() {
@@ -48,22 +47,30 @@ EcoPlugPlatform.prototype.didFinishLaunching = function() {
   eco.startUdpServer(this, function(message) {
     // handle status messages received from devices
 
-    var accessory = this.accessories[message.id];
+    var accessory = accessories[message.id];
 
     accessory.getService(Service.Outlet)
       .getCharacteristic(Characteristic.On)
       .updateValue(message.status);
 
   });
+
+  debug("HAPServer",HAPServer);
+
+  HAPServer.emit('accessories', function(err, accessories) {
+    JSON.stringify(accessories);
+  });
+
   this.deviceDiscovery();
-  setInterval(that.devicePolling.bind(this), this.refresh * 1000);
-  setInterval(that.deviceDiscovery.bind(this), this.cache_timeout * 6000);
+  setInterval(this.devicePolling.bind(this), this.refresh * 1000);
+  setInterval(this.deviceDiscovery.bind(this), this.cache_timeout * 6000);
+
 }
 
 EcoPlugPlatform.prototype.devicePolling = function() {
   // Send a return status message every interval
-  for (var id in this.accessories) {
-    var plug = this.accessories[id];
+  for (var id in accessories) {
+    var plug = accessories[id];
 
     debug("Poll:", id, plug.context.name);
     this.sendStatusMessage(plug.context);
@@ -83,7 +90,7 @@ EcoPlugPlatform.prototype.deviceDiscovery = function() {
       debug("Adding discovered devices");
 
       for (var i in devices) {
-        var existing = this.accessories[devices[i].id];
+        var existing = accessories[devices[i].id];
 
         if (!existing) {
           this.log("Adding:", devices[i].id, devices[i].name, devices[i].host);
@@ -104,7 +111,7 @@ EcoPlugPlatform.prototype.deviceDiscovery = function() {
 }
 
 EcoPlugPlatform.prototype.addAccessory = function(data) {
-  if (!this.accessories[data.id]) {
+  if (!accessories[data.id]) {
     var uuid = UUIDGen.generate(data.id);
 
     var accessory = new Accessory(data.id, uuid, 8);
@@ -126,19 +133,18 @@ EcoPlugPlatform.prototype.addAccessory = function(data) {
 
     this.api.registerPlatformAccessories("homebridge-ecoplugs", "EcoPlug", [accessory]);
   } else {
-    var accessory = this.accessories[data.id];
+    var accessory = accessories[data.id];
   }
 
-  this.accessories[data.id] = accessory;
+  accessories[data.id] = accessory;
 }
 
 EcoPlugPlatform.prototype.removeAccessory = function(accessory) {
   if (accessory) {
-    var name = accessory.context.name;
     var id = accessory.context.id;
-    this.log("Removing EcoPlug: " + name);
+    this.log("Removing EcoPlug: " + accessory.context.name);
     this.api.unregisterPlatformAccessories("homebridge-ecoplugs", "EcoPlug", [accessory]);
-    delete this.accessories[id];
+    delete accessories[id];
   }
 }
 
@@ -177,7 +183,7 @@ EcoPlugPlatform.prototype.sendStatusMessage = function(thisPlug, callback) {
   eco.sendMessage(this, message, thisPlug, retry_count, function(err, message) {
     if (err) {
       this.log.error("Error: sendStatusMessage", thisPlug.id, err.message);
-      this.accessories[thisPlug.id].getService(Service.Outlet)
+      accessories[thisPlug.id].getService(Service.Outlet)
         .getCharacteristic(Characteristic.On)
         .updateValue(new Error("Polling failed"));
       if (callback) {
@@ -193,11 +199,11 @@ EcoPlugPlatform.prototype.sendStatusMessage = function(thisPlug, callback) {
 
 EcoPlugPlatform.prototype.identify = function(thisPlug, paired, callback) {
   this.log("Identify requested for " + thisPlug.id, thisPlug.name);
-  if (this.accessories[thisPlug.id]) {
+  if (accessories[thisPlug.id]) {
     this.sendStatusMessage(thisPlug, function(err) {
       if (err) {
         debug("Identity - Not Found");
-        this.removeAccessory(this.accessories[thisPlug.id]);
+        this.removeAccessory(accessories[thisPlug.id]);
       } else {
         debug("Identity - Found");
       }
